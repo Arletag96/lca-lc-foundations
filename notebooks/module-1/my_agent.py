ed0b8eee700c2321531e17021e37fbe1249e43b4
Arletag96 <arletacostagonzalez@gmail.com>
2026-06-30 17:45:38 +0200

    cheff

---
+++ notebooks/module-1/my_agent.py
@@ -0,0 +1,46 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from langchain.tools import tool
+from typing import Dict, Any
+from tavily import TavilyClient
+from langchain.agents import create_agent
+
+
+
+@tool
+def web_search(query: str) -> Dict[str, Any]:
+
+    """Search the web for information"""
+
+    return tavily_client.search(query)
+
+system_prompt = """
+
+You are a personal chef. The user will give you a list of ingredients they have left over in their house.
+
+Using the web search tool, search the web for recipes that can be made with the ingredients they have.
+
+Return recipe suggestions and eventually the recipe instructions to the user, if requested.
+
+"""
+#inicialización de un agente
+
+agent = create_agent(
+    model="gpt-5-nano",
+    tools=[web_search],
+    system_prompt=system_prompt
+)
+
+
+tavily_client = TavilyClient()
+
+@tool
+def web_search(query: str) -> Dict[str, Any]:
+
+    """Search the web for information"""
+
+    return tavily_client.search(query)
+
+web_search.invoke("Cómo puedo hacer jabón?")
